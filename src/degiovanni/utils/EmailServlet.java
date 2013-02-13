40f93f185ceef7f37fb83bc1846350408f54184e
Joe Degiovanni <joe.degiovanni@gmail.com>
2013-02-12 19:32:59 -0600

    Adding Email Servlet

---
+++ src/degiovanni/utils/EmailServlet.java
@@ -0,0 +1,44 @@
+package degiovanni.utils;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class EmailServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		    throws IOException, ServletException
+		    {
+		        response.setContentType("text/html");
+		        PrintWriter out = response.getWriter();
+		        out.println("<html>");
+		        out.println("<head>");
+		        out.println("<title>Email Servlet</title>");
+		        out.println("</head>");
+		        out.println("<body>");
+		        out.println("<h1>Email Servlet</h1>");
+		        out.println("</body>");
+		        out.println("</html>");
+		    }
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		    throws IOException, ServletException
+		    {
+		        response.setContentType("text/html");
+		        PrintWriter out = response.getWriter();
+		        out.println("<html>");
+		        out.println("<head>");
+		        out.println("<title>Email Servlet</title>");
+		        out.println("</head>");
+		        out.println("<body>");
+		        out.println("<h1>Email Servlet</h1>");
+		        out.println("</body>");
+		        out.println("</html>");
+		    }
+
+}
